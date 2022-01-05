07fe7745211717e6a6824de996c235c8648cc58e
renatogm24 <renato.garay@pucp.pe>
2022-01-05 17:01:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function login(element) {
+    element.value = "Logout";
+}
+
+function removeDefinition(element) {
+    element.remove();
+}
+
+function alertWord() {
+    alert("Ninja was liked");
+}
